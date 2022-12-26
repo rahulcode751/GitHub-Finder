2aa1861e52b6cbd8fc6eb862356af647137cf766
Rahul Bairagi <rahulbairagiofficial@gmail.com>
2022-12-26 20:28:52 +0530

    UsersList taken

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 /** @format */
+import UserResults from "../components/users/UserResults";
 
 function Home() {
   return (
-    <div>
-      <h1 className='text-6xl'>Welcome</h1>
-    </div>
+    // <div>
+    //   {/* <h1 className='text-6xl'>Welcome</h1> */}
+    //   {UserResults}
+    // </div>
+    <>
+      {/* SEARCH COMPONENT */}
+      <UserResults />
+    </>
   );
 }
 
